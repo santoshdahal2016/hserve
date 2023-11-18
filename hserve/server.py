71735675ef0f95c3f4ba0b584bfe681ffdb26c70
santoshdahal2016 <webtutor2016@gmail.com>
2023-11-18 20:55:12 +0545

    basic static serve implemented

--- hserve/server.py
+++ hserve/server.py
@@ -10,6 +10,50 @@ class HTTPError(Exception):
         self.message = message
         super().__init__(f"{status_code} {message}")
 
+def handle_request(request):
+    try:
+        rows = request.split("\r\n")
+        print(rows[0])
+        method, path, _ = rows[0].split(" ")
+        path = path.lstrip("/")
+        is_directory = os.path.isdir(path)
+
+        if path == "" or is_directory:
+            if is_directory:
+                if os.path.exists(f"{path}/index.html"):
+                    path = f"{path}/index.html"
+                else: 
+                    file_list = os.listdir(path)
+                    links = "\n".join(f'<a href="/{file}">{file}</a> <br>' for file in file_list)
+                    content = f"<html><body>{links}</body></html>".encode()
+                    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n".encode() + content
+                    return response
+            else:
+                if os.path.exists(f"index.html"):
+                    path = f"index.html"
+                else: 
+                    current_directory = os.getcwd()
+                    file_list = os.listdir(current_directory)
+                    links = "\n".join(f'<a href="/{file}">{file}</a> <br>' for file in file_list)
+                    content = f"<html><body>{links}</body></html>".encode()
+                    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n".encode() + content
+                    return response       
+        
+        if os.path.exists(path):
+            with open(f"{path}", "rb") as file:
+                content = file.read()
+                response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n".encode() + content
+
+        else:
+            response_content = f"File or Directory not found".encode()
+            response = f"HTTP/1.1 404 Not Found\r\nContent-Length: {len(response_content)}\r\n\r\n".encode() + response_content
+    
+
+    except Exception as e:
+        print(f"Error handling request: {e}")
+        response = b"HTTP/1.1 500 Internal Server Error\r\n\r\n500 Internal Server Error"
+
+    return response
 
 def start_server():
     HOST = "localhost"
@@ -43,8 +87,8 @@ def start_server():
                 if method != "GET":
                     raise HTTPError(405, "Method not allowed")
                 
-                response = f"HTTP/1.1 {200} OK\r\nContent-Type: text/html\r\n\r\n You are trying to access: {path}"
-                client_socket.sendall(response.encode("utf-8"))
+                response = handle_request(request)
+                client_socket.sendall(response)
     except KeyboardInterrupt:
 
         shutdown(signal.SIGINT, None)
@@ -52,4 +96,5 @@ def start_server():
         listener.close()
 
 if __name__ == "__main__":
+    os.chdir(os.getcwd())
     start_server()
